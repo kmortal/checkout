92e45bbf1ba2becb68de43917468a657cabdb806
kmortal <kelvynmoraes2004@gmail.com>
2022-11-17 02:17:16 +0000

    finalization

--- main.js
+++ main.js
@@ -5,34 +5,31 @@ const items1 = document.querySelector('#vintageBackbag')
 const items2 = document.querySelector('#leviShoes')
 const total = document.querySelector('#total')
 
-//let sum = 0
-//fazer o total
-//fazer o click enter
-//fazer a validação
-
 buttonUpper[0].onclick = (e)=>Checkout(buttonUpper[0], e)
 buttonUpper[1].onclick = (e)=>Checkout(buttonUpper[1], e)
 
 buttonLower[0].onclick = (e)=>Checkout(buttonLower[0], e)
 buttonLower[1].onclick = (e)=>Checkout(buttonLower[1], e)
 
+let sum = 0
 function Checkout(element, e){
     const product = e.target.parentNode.parentNode.parentNode.firstElementChild.outerText
     
     if(element.className == 'upper'){
-        console.log(element)
-        
         if (product == 'Vintage Backbag') {
             let current1 = Number(items1.innerText)
             current1 += 1
             items1.innerText = current1
             sum+=54.99
+            showTotal(sum)
             return
         }
+        console.log(element)
         let current2 = Number(items2.innerText)
         current2 += 1
         items2.innerText = current2
         sum+=74.99
+        showTotal(sum)
     }
 
     else{
@@ -41,6 +38,7 @@ function Checkout(element, e){
             current1 -= 1
             items1.innerText = current1
             sum-=54.99
+            showTotal(sum)
             return
         }
         if(items2.innerText >=1){
@@ -48,8 +46,36 @@ function Checkout(element, e){
             current2 -= 1
             items2.innerText = current2
             sum-=74.99
+            showTotal(sum)
         }
     }
-
-    console.log(currentPrice)
 }
+
+function showTotal(num){
+    let TOTAL = num.toFixed(2)
+    total.innerText = `$${Math.abs(TOTAL)}`
+}
+
+function validateForm(){
+    const inputsForm = document.querySelectorAll('input[type=text]')
+    let validated = true;
+    inputsForm.forEach(input => {
+        if(!input.value){
+            validated = false
+        }
+    })
+    if(validated){
+        alert('Validação bem sucedida')
+        return
+    }
+    alert('Validação não sucedida. Todos os campos devem ser preenchidos')
+}
+
+
+const body = document.getElementsByTagName('body')
+body[0].onkeydown = (e)=>{
+    if(e.key == 'Enter') validateForm()
+}
+
+
+//fazer o click enter
